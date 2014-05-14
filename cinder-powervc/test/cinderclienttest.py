fd55c64dad0215f9ac792b652c3a6aabac50532c
Christian Berendt <berendt@b1-systems.de>
2014-05-14 19:35:40 +0200

    method print should be used instead of print statement
    Change-Id: I30d8bb3f9e7db342c2dce865823f094cb0c5ee00

--- cinder-powervc/test/cinderclienttest.py
+++ cinder-powervc/test/cinderclienttest.py
@@ -6,6 +6,8 @@ Simple cinder client tests
 TODO: Convert to pyunit and use config file
 """
 
+from __future__ import print_function
+
 import powervc.common.constants as constants
 
 from powervc.common import config
@@ -15,8 +17,8 @@ import powervc.common.client.factory as clients
 
 cinder_client = clients.POWERVC.new_client(str(constants.SERVICE_TYPES.volume))
 
-print '=' * 10, 'Listing volumes', '=' * 10
+print('=' * 10, 'Listing volumes', '=' * 10)
 vol_list = cinder_client.volumes.list()
 for vol in vol_list:
-    print str(vol.display_name), str(vol.display_description), \
-        vol.id
+    print(str(vol.display_name), str(vol.display_description),
+          vol.id)
