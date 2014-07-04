7377f6075b8ff77a62f6fb6fd80cbcc5588b62ed
Eric_Zhao <jianzj@cn.ibm.com>
2014-07-03 22:34:14 -0400

    This script file is used to intergrate every single file named "run _tests.sh" in each "*-powervc" folder. Use the new file to run tests and treat the powervc-driver as a whole project.
    Change-Id: Iddeb0fe3bb2455c909ad6d8e6c4dcbb479c08838

---
+++ run_tests.sh
@@ -0,0 +1,206 @@
+#!/bin/bash
+
+# Copyright 2012 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+set -eu
+
+function usage {
+  echo "Usage: $0 [OPTION]..."
+  echo "Run PowerVC Driver test suite(s)"
+  echo ""
+  echo "  -V, --virtual-env        Always use virtualenv.  Install automatically if not present"
+  echo "  -N, --no-virtual-env     Don't use virtualenv.  Run tests in local environment"
+  echo "  -r, --recreate-db        Recreate the test database (deprecated, as this is now the default)."
+  echo "  -n, --no-recreate-db     Don't recreate the test database."
+  echo "  -x, --stop               Stop running tests after the first error or failure."
+  echo "  -f, --force              Force a clean re-build of the virtual environment. Useful when dependencies have been added."
+  echo "  -u, --update             Update the virtual environment with any newer package versions"
+  echo "  -p, --pep8               Just run flake8"
+  echo "  -8, --8                  Just run flake8, don't show PEP8 text for each error"
+  echo "  -P, --no-pep8            Don't run flake8"
+  echo "  -c, --coverage           Generate coverage report"
+  echo "  -h, --help               Print this usage message"
+  echo "  --hide-elapsed           Don't print the elapsed time for each test along with slow test list"
+  echo "  --standard-threads       Don't do the eventlet threading monkeypatch."
+  echo ""
+  echo "Note: with no options specified, the script will try to run the tests in a virtual environment,"
+  echo "      If no virtualenv is found, the script will ask if you would like to create one.  If you "
+  echo "      prefer to run tests NOT in a virtual environment, simply pass the -N option."
+  exit
+}
+
+function process_option {
+  case "$1" in
+    -h|--help) usage;;
+    -V|--virtual-env) always_venv=1; never_venv=0;;
+    -N|--no-virtual-env) always_venv=0; never_venv=1;;
+    -r|--recreate-db) recreate_db=1;;
+    -n|--no-recreate-db) recreate_db=0;;
+    -f|--force) force=1;;
+    -u|--update) update=1;;
+    -p|--pep8) just_flake8=1;;
+    -8|--8) short_flake8=1;;
+    -P|--no-pep8) no_flake8=1;;
+    -c|--coverage) coverage=1;;
+    --standard-threads)
+        export STANDARD_THREADS=1
+        ;;
+    -*) noseopts="$noseopts $1";;
+    *) noseargs="$noseargs $1"
+  esac
+}
+
+venv=.venv
+with_venv=tools/with_venv.sh
+always_venv=0
+never_venv=0
+force=0
+noseargs=
+noseopts=
+wrapper=""
+just_flake8=0
+short_flake8=0
+no_flake8=0
+coverage=0
+recreate_db=1
+update=0
+
+for arg in "$@"; do
+  process_option $arg
+done
+
+
+function run_tests {
+  # Just run the test suites in current environment
+
+  COMPONENTS=(`ls | grep powervc`)
+  NOSETESTS_TARGETEDDIR=""
+  for COMPONENT in ${COMPONENTS[@]} ; do
+    NOSETESTS_TARGETEDDIR=${NOSETESTS_TARGETEDDIR}" $COMPONENT/"
+  done
+  ${wrapper} $NOSETESTS $NOSETESTS_TARGETEDDIR
+  # If we get some short import error right away, print the error log directly
+
+  #ARGETED_COMPONENTS=(`ls | grep powervc`)
+  #or TARGETED_COMPONENT in ${TARGETED_COMPONENTS[@]} ; do
+  # ${wrapper} $NOSETESTS $TARGETED_COMPONENT/
+  #one
+
+  RESULT=$?
+  if [ "$RESULT" -ne "0" ];
+  then
+    ERRSIZE=`wc -l run_tests.log | awk '{print \$1}'`
+    if [ "$ERRSIZE" -lt "40" ];
+    then
+        cat run_tests.log
+    fi
+  fi
+  return $RESULT
+}
+
+
+function run_flake8 {
+  FLAGS=--show-pep8
+  if [ $# -gt 0 ] && [ 'short' == ''$1 ]
+  then
+      FLAGS=''
+  fi
+
+
+  echo "Running flake8 ..."
+  # Just run flake8 in current environment
+  #echo ${wrapper} flake8 $FLAGS $commonent/powervc | tee pep8.txt
+  TARGETED_COMPONENTS=(`ls | grep powervc`)
+  for TARGETED_COMPONENT in ${TARGETED_COMPONENTS[@]} ; do
+    ${wrapper} flake8 $FLAGS $TARGETED_COMPONENT/powervc | tee pep8.txt
+  done
+  RESULT=${PIPESTATUS[0]}
+  return $RESULT
+}
+
+NOSETESTS="nosetests $noseopts $noseargs"
+
+if [ $never_venv -eq 0 ]
+then
+  # Remove the virtual environment if --force used
+  if [ $force -eq 1 ]; then
+    echo "Cleaning virtualenv..."
+    rm -rf ${venv}
+  fi
+  if [ $update -eq 1 ]; then
+    echo "Updating virtualenv..."
+    python tools/install_venv.py
+  fi
+  if [ -e ${venv} ]; then
+    wrapper="${with_venv}"
+  else
+    if [ $always_venv -eq 1 ]; then
+      # Automatically install the virtualenv
+      python tools/install_venv.py
+      wrapper="${with_venv}"
+    else
+      echo -e "No virtual environment found...create one? (Y/n) \c"
+      read use_ve
+      if [ "x$use_ve" = "xY" -o "x$use_ve" = "x" -o "x$use_ve" = "xy" ]; then
+        # Install the virtualenv and run the test suite in it
+        python tools/install_venv.py
+        wrapper=${with_venv}
+      fi
+    fi
+  fi
+fi
+
+if [ $just_flake8 -eq 1 ]; then
+	run_flake8
+    RESULT=$?
+    echo "RESULT $RESULT"
+    exit $RESULT
+fi
+
+if [ $short_flake8 -eq 1 ]; then
+	run_flake8 short
+    RESULT=$?
+    exit $RESULT
+fi
+
+run_tests
+RESULT=$?
+
+
+# NOTE(sirp): we only want to run flake8 when we're running the full-test
+# suite, not when we're running tests individually. To handle this, we need to
+# distinguish between options (noseopts), which begin with a '-', and arguments
+# (noseargs).
+if [ -z "$noseargs" ]; then
+  if [ $no_flake8 -eq 0 ]; then
+	run_flake8
+	TMP_RESULT=$?
+    RESULT=$(($TMP_RESULT + $RESULT))
+  fi
+fi
+
+if [ $coverage -eq 1 ]; then
+    echo "Generating coverage report in covhtml/"
+    ${wrapper} coverage html -d covhtml -i
+fi
+
+exit $RESULT
+
+
+
+
+
+
+
