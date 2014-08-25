c025d2012ec7fd1f7e23192aee34e17d2e7cfc2e
Le Tian Ren <renlet@cn.ibm.com>
2014-08-25 18:46:04 +0800

    New config strategy to adapt to oslo-messaging
    * Extract and remove --config-file amqp_openstack.conf(or
      amqp-openstack-neutron and --config-file amqp-powervc.conf from sys.argv to
      create 2 AMQP cfg.ConfigOpts objects without conflicting with other options
      parsing as before, particularly those OpenStack AMQP configurations in
      [DEFAULT] section of nova.conf, cinder.conf.
    
    * adapt all affected code to the new config strategy
    
    Implements: blueprint https://blueprints.launchpad.net/powervc-driver/+spec/new-configuration-strategy-needed-to-adapt-to-oslo-messaging
    
    Closes-Bug: 1356225
    Change-Id: I61a4fd0c28148e31a8cb85acea3793f40028dfb4

--- common-powervc/powervc/common/config.py
+++ common-powervc/powervc/common/config.py
@@ -1,17 +1,58 @@
-# Copyright 2013 IBM Corp.
+# Copyright 2013, 2014 IBM Corp.
 
 """Config file utility
 
 """
-import constants
+import os
+import sys
+
+from powervc.common import constants
 
 from oslo.config import cfg
+
+if 'powervc' in sys.argv[0]:
+    # Extract and remove --config-file amqp_openstack.conf and --config-file
+    # amqp-powervc.conf from sys.argv to create 2 AMQP cfg.ConfigOpts objects
+    # without conflicting with other options parsing as before, particularly
+    # those OpenStack AMQP configurations in [DEFAULT] section of nova.conf,
+    # cinder.conf.
+    argv = sys.argv[1:]
+    argv1 = None
+    argv2 = None
+    argv1_index = -1
+    argv2_index = -1
+    for i in range(len(argv)):
+        if argv[i] == '--config-file':
+            # for amqp-openstack.conf or amqp-openstack-neutron.conf
+            if 'amqp-openstack' in os.path.basename(argv[i+1]):
+                argv1 = argv[i:i+2]
+                argv1_index = i+1
+            elif os.path.basename(argv[i+1]) == 'amqp-powervc.conf':
+                argv2 = argv[i:i+2]
+                argv2_index = i+1
+    if argv1 is None or argv2 is None:
+        sys.exit("Error configuration.")
+    else:
+        sys.argv = (sys.argv[:argv1_index]
+                    + sys.argv[argv1_index+2:argv2_index]
+                    + sys.argv[argv2_index+2:])
+
+    AMQP_OPENSTACK_CONF = cfg.ConfigOpts()
+    AMQP_POWERVC_CONF = cfg.ConfigOpts()
+    AMQP_OPENSTACK_CONF(args=argv1)
+    AMQP_POWERVC_CONF(args=argv2)
+    # print(dict(AMQP_OPENSTACK_CONF))
+    # print(sys.argv)
+    from oslo.messaging import transport
+    trans_os = transport.get_transport(AMQP_OPENSTACK_CONF)
+    trans_pvc = transport.get_transport(AMQP_POWERVC_CONF)
+    # print(dict(AMQP_OPENSTACK_CONF))
+
 CONF = cfg.CONF
 
 
 def parse_power_config(argv, base_project, base_prog=None):
-    """
-    Loads configuration information from powervc.conf as well as a project
+    """Loads configuration information from powervc.conf as well as a project
     specific file.  Expectation is that all powervc config options will be in
     the common powervc.conf file and the base_project will represent open stack
     component configuration like nova.conf or cinder.conf. A base_prog file
@@ -54,12 +95,7 @@ FILE_OPTIONS = {
         cfg.StrOpt('region_name', default=None),
         cfg.IntOpt('keystone_max_try_times', default=30),
         cfg.IntOpt('keystone_retry_interval', default=2),
-        # Hosting OS Qpid connection info
-        cfg.StrOpt('qpid_hostname'),
-        cfg.IntOpt('qpid_port', default=5672),
-        cfg.StrOpt('qpid_username', default='anonymous'),
-        cfg.StrOpt('qpid_password', secret=True, default=''),
-        cfg.StrOpt('qpid_protocol', default='tcp')],
+    ],
     'powervc': [
         # Keystone info
         cfg.StrOpt('auth_url', default='http://localhost:5000/v2.0/'),
@@ -72,12 +108,6 @@ FILE_OPTIONS = {
         cfg.BoolOpt('http_insecure', default=False),
         cfg.StrOpt('keystone_version', default="v3"),
         cfg.IntOpt('expiration_stale_duration', default=3600),
-        # Hosting OS Qpid connection info
-        cfg.StrOpt('qpid_hostname'),
-        cfg.IntOpt('qpid_port', default=5672),
-        cfg.StrOpt('qpid_username', default='anonymous'),
-        cfg.StrOpt('qpid_password', secret=True, default=''),
-        cfg.StrOpt('qpid_protocol', default='tcp'),
         # manager
         cfg.StrOpt('powervc_manager',
                    default='powervc.compute.manager.PowerVCCloudManager'),
