2ec10e351105bdd1e023b943303c53b318a5c9fd
Jerry Cai <caimin@cn.ibm.com>
2014-10-27 11:08:40 +0800

    Add nova CLI extension for host maintenance mode status query/evaucation
    Add a nova cli "nova host-maintenance" to query and enable/disable
    the host maintenance mode for a specified host.
    
    For detail usage, please refer to: "nova help host-maintenance".
    
    Change-Id: Ibc3f92521546a82672b1619f7b65416909d92dfc
    Closes-Bug: 1386026

---
+++ nova-powervc/hostmaintenanceclient/setup.py
@@ -0,0 +1,15 @@
+# Copyright 2013 IBM Corp.
+
+from setuptools import setup, find_packages
+
+setup(
+    name="hostmaintenance-client",
+    version="0.1",
+    packages=find_packages(exclude=['*.tests', 'tests',
+                                    'tests.*', '*.tests.*']),
+    entry_points={
+        'novaclient.extension': [
+            'host_maintenance = v1_1.host_maintenance',
+        ],
+    }
+)
