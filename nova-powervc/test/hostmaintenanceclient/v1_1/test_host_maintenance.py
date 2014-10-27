ff1a346dc3b425fa146dbf44ebe7991c5e920760
Jerry Cai <caimin@cn.ibm.com>
2014-10-27 11:08:40 +0800

    Add nova CLI extension for host maintenance mode status query/evaucation
    Add a nova cli "nova host-maintenance" to query and enable/disable
    the host maintenance mode for a specified host.
    
    For detail usage, please refer to: "nova help host-maintenance".
    
    Change-Id: Ibc3f92521546a82672b1619f7b65416909d92dfc
    Closes-Bug: 1386026
    (cherry picked from commit 40e90593e3ec084509a8fb1df346ecab894a6514)

---
+++ nova-powervc/test/hostmaintenanceclient/v1_1/test_host_maintenance.py
@@ -0,0 +1,142 @@
+# Copyright 2014 IBM Corp.
+
+from webob import exc
+
+from mock import MagicMock
+from novaclient import exceptions
+from novaclient.tests.fixture_data import client
+from novaclient.tests.fixture_data import hypervisors as data
+from novaclient.tests import utils
+from hostmaintenanceclient.v1_1 import host_maintenance
+
+
+class HostMaintenanceTest(utils.FixturedTestCase):
+    client_fixture_class = client.V1
+    data_fixture_class = data.V1
+
+    def compare_to_expected(self, expected, hyper):
+        for key, value in expected.items():
+            self.assertEqual(getattr(hyper, key), value)
+
+    def test_host_maintenance_get(self):
+        restAPI = MagicMock()
+        args = MagicMock()
+        # generate cs parameter
+        get_return_value = {"maintenance_status": "on",
+                            "maintenance_migration_action": "none"}
+
+        host_manager = host_maintenance.HostMaintenanceManager(restAPI)
+        host_manager.api.client.get = MagicMock(
+            return_value=(200, get_return_value))
+        self.cs.host_maintenance = host_manager
+        args.set_status = None
+        args.migrate = None
+        args.target_host = None
+        args.host = "789522X_067E30B"
+
+        # generate args parameter
+        host_maintenance.do_host_maintenance(self.cs, args)
+        restAPI.client.get.assert_called_once_with(
+            '/os-host-maintenance-mode/789522X_067E30B')
+
+    def test_host_maintenance_get_not_found(self):
+        restAPI = MagicMock()
+        args = MagicMock()
+        # generate cs parameter
+        host_manager = host_maintenance.HostMaintenanceManager(restAPI)
+        host_manager.api.client.get = MagicMock(
+            side_effect=exc.HTTPNotFound('The specified host cannot be found'))
+        self.cs.host_maintenance = host_manager
+        args.set_status = None
+        args.migrate = None
+        args.target_host = None
+        args.host = "789522X_067E30B"
+
+        # generate args parameter
+        self.assertRaises(exc.HTTPNotFound,
+                          host_maintenance.do_host_maintenance,
+                          self.cs, args)
+        restAPI.client.get.assert_called_once_with(
+            '/os-host-maintenance-mode/789522X_067E30B')
+
+    def test_host_maintenance_update_enable_success(self):
+        restAPI = MagicMock()
+        args = MagicMock()
+        # generate cs parameter
+        update_return_value = {"hypervisor_maintenance":
+                               {"status": "enable",
+                                "migrate": "none",
+                                "target-host": "none",
+                                "hypervisor_hostname": "789522X_067E30B"}
+                               }
+
+        host_manager = host_maintenance.HostMaintenanceManager(restAPI)
+        host_manager.api.client.put = MagicMock(
+            return_value=(200, update_return_value))
+        self.cs.host_maintenance = host_manager
+        args.set_status = 'enable'
+        args.migrate = None
+        args.target_host = None
+        args.host = "789522X_067E30B"
+
+        # generate args parameter
+        host_maintenance.do_host_maintenance(self.cs, args)
+        restAPI.client.put.assert_called_once_with(
+            '/os-host-maintenance-mode/789522X_067E30B',
+            body={'status': 'enable', 'migrate': None, 'target_host': None})
+
+    def test_host_maintenance_update_disable_success(self):
+        restAPI = MagicMock()
+        args = MagicMock()
+        # generate cs parameter
+        update_return_value = {"hypervisor_maintenance":
+                               {"status": "disable",
+                                "migrate": "none",
+                                "target-host": "none",
+                                "hypervisor_hostname": "789522X_067E30B"}
+                               }
+
+        host_manager = host_maintenance.HostMaintenanceManager(restAPI)
+        host_manager.api.client.put = MagicMock(
+            return_value=(200, update_return_value))
+        self.cs.host_maintenance = host_manager
+        args.set_status = 'disable'
+        args.migrate = None
+        args.target_host = None
+        args.host = "789522X_067E30B"
+
+        # generate args parameter
+        host_maintenance.do_host_maintenance(self.cs, args)
+        restAPI.client.put.assert_called_once_with(
+            '/os-host-maintenance-mode/789522X_067E30B',
+            body={'status': 'disable'})
+
+    def test_host_maintenance_get_parameter_error1(self):
+        args = MagicMock()
+        args.set_status = None
+        args.migrate = 'active-only'
+        args.target_host = None
+        args.host = "789522X_067E30B"
+
+        self.assertRaises(exceptions.CommandError,
+                          host_maintenance.do_host_maintenance, self.cs, args)
+
+    def test_host_maintenance_get_parameter_error2(self):
+        args = MagicMock()
+        args.set_status = None
+        args.migrate = None
+        args.target_host = "789522X_067E31B"
+        args.host = "789522X_067E30B"
+
+        self.assertRaises(exceptions.CommandError,
+                          host_maintenance.do_host_maintenance, self.cs, args)
+
+    def test_host_maintenance_get_parameter_error3(self):
+        args = MagicMock()
+        args.set_status = 'disable'
+        args.migrate = None
+        args.target_host = "789522X_067E31B"
+        args.host = "789522X_067E30B"
+
+        self.assertRaises(exceptions.CommandError,
+                          host_maintenance.do_host_maintenance, self.cs, args)
