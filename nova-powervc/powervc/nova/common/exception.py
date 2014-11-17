661a44feb8127b4f57940dc93f29557e7c5fb58d
terryyao <yaohaif@cn.ibm.com>
2014-11-18 01:40:28 +0800

    Fix infinite loop wait for live-migration completion
    PowerVC will set VM state to "Active" if the live migration failed as
    the source host or dest host is running live migration. With our current
    code, we are looping wait for the migration to be completed until the
    powervc instance's host has been changed. That's definitely wrong..
    
    Closes-Bug: #1393663
    Change-Id: Ifb826bafa692b51f3d5997d427e10fbe17a21ca6
    (cherry picked from commit 4c72a1fdba4d671d4c302c274fda213b2dc1ced5)

--- nova-powervc/powervc/nova/common/exception.py
+++ nova-powervc/powervc/nova/common/exception.py
@@ -9,3 +9,9 @@ class BlockMigrationException(exception.NovaException):
     def __init__(self):
         message = _("PowerVC does not support block migration.")
         super(BlockMigrationException, self).__init__(message=message)
+
+class LiveMigrationException(exception.NovaException):
+    """User attempted to perform live migration with block migration."""
+    def __init__(self, uuid):
+        message = 'PowerVC can not live migrate the instance %s' % uuid
+        super(LiveMigrationException, self).__init__(message=message)
