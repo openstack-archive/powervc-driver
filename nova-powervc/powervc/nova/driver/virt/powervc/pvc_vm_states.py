82ab8dfb38d02b85226c8402b6dc01085297a11b
Jerry Cai <caimin@cn.ibm.com>
2015-01-09 13:38:58 +0800

    Failed to sync nova-powervc as kilo source codes change
    Provides some fixes for kilo nova base code changed.
    
    Change-Id: I2b7da3b688e88084939f10cf060c2b7ecc5e0a55
    Closes-Bug:1408888

--- nova-powervc/powervc/nova/driver/virt/powervc/pvc_vm_states.py
+++ nova-powervc/powervc/nova/driver/virt/powervc/pvc_vm_states.py
@@ -7,3 +7,23 @@ SHUTOFF = u'SHUTOFF'
 RESIZE = u'RESIZE'
 VERIFY_RESIZE = u'VERIFY_RESIZE'
 MIGRATING = u' MIGRATING'
+
+
+class InstanceInfo(object):
+
+    def __init__(self, state=None, max_mem_kb=0, mem_kb=0, num_cpu=0,
+                 cpu_time_ns=0):
+        """Create a new Instance Info object
+
+        :param state: the running state, one of the power_state codes
+        :param max_mem_kb: (int) the maximum memory in KBytes allowed
+        :param mem_kb: (int) the memory in KBytes used by the instance
+        :param num_cpu: (int) the number of virtual CPUs for the instance
+        :param cpu_time_ns: (int) the CPU time used in nanoseconds
+        :param id: a unique ID for the instance
+        """
+        self.state = state
+        self.max_mem_kb = max_mem_kb
+        self.mem_kb = mem_kb
+        self.num_cpu = num_cpu
+        self.cpu_time_ns = cpu_time_ns
