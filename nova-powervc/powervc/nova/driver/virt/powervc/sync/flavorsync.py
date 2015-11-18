b2cde5f8ef47021a5106d4857fa6198ccba2fc2f
Qing Wu Wang <wangqwsh@cn.ibm.com>
2015-11-18 00:06:40 -0600

    Change the default value of disk size to 1GB.
    Change the default value of disk size to 1GB.
    
    Change-Id: I3e488eae4b4cbd6f0c7f5b874fade90e923fa3ae
    Closes-Bug: #1517319

--- nova-powervc/powervc/nova/driver/virt/powervc/sync/flavorsync.py
+++ nova-powervc/powervc/nova/driver/virt/powervc/sync/flavorsync.py
@@ -187,13 +187,16 @@ class FlavorSync():
         memory = flavor.ram
         vcpus = flavor.vcpus
         '''
-        RTC 213328 - flavor is sync from PowerVC.
+        RTC 213328 - flavor is sync from PowerVC 1.3.
         The disk attribute of flavor is set ZERO by default. It would impact
-        the validation of resizing instance. Temporarily, setting the size is 10000 to
+        the validation of resizing instance. Temporarily, setting the size is 1GB to
         pass the resizing validation.
         '''
-        #root_gb = flavor.disk
-        root_gb = 10000
+        if flavor.disk is None or 0 == flavor.disk:
+            root_gb = 1
+        else:
+            root_gb = flavor.disk
+
         ephemeral_gb = flavor_dict.get('OS-FLV-EXT-DATA:ephemeral', 0)
         u_swap = flavor_dict.get('swap', 0)
         rxtx_factor = flavor_dict.get('rxtx_factor', 1.0)
